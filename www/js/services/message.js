25335bebc5a794d11c92e8ee995a8f94e09e34ed
maatumo <Yoshihiko@Mattsbook-pro.local>
2016-05-28 03:23:07 +0900

    add callback for getObjectUserName

--- www/js/services/message.js
+++ www/js/services/message.js
@@ -58,10 +58,10 @@ app.factory('Message', function(FURL, $firebaseArray, $firebaseObject, Auth, Wan
                   var newRoom={
                     users: [uid1,uid2]
                   };
-                  
+
                   Auth.getUsersRooms(uid1).$loaded().then(function(data){
                     if(_.contains(_.pluck(data,'friendId'),uid2) ){//message room を複数作らないための処理。roomsの下のfriendIdのみを取り出してリスト化する。その上で、uid2が含まれているかどうかを調べる。
-  
+
                       console.log('this room is already added');
 
                     }
@@ -71,40 +71,49 @@ app.factory('Message', function(FURL, $firebaseArray, $firebaseObject, Auth, Wan
                         console.log('roomId is',roomId);
                         var user1 = $firebaseArray(ref.child('users').child(uid1).child('rooms'));
                         var user2 = $firebaseArray(ref.child('users').child(uid2).child('rooms'));
-                        
-                        var newRoom1 = {
-                          roomId: roomId,
-                          friendId: uid2, 
-                          friendName: Wannas.getUserName(uid2)
 
-                        };
-                        
 
-                        return user1.$add(newRoom1).then(function(){
-                          var newRoom2 = {
-                          roomId: roomId,
-                          friendId: uid1,
-                          friendName: Wannas.getUserName(uid1)
-                          };
-                          
-                          return user2.$add(newRoom2).then(function(){
-                            
-                            var newMessage={
-                                message: message,
-                                userId: uid1
+                        return Wannas.getObjectUserName(uid2).$loaded().then(function(obj2){
+                          var userName2=obj2.$value;
+                            var newRoom1 = {
+                              roomId: roomId,
+                              friendId: uid2,
+                              friendName: userName2
+
+                            };
+
+                            return user1.$add(newRoom1).then(function(){
+                              return Wannas.getObjectUserName(uid1).$loaded().then(function(obj1){
+                                var userName1=obj1.$value;
+                                var newRoom2 = {
+                                roomId: roomId,
+                                friendId: uid1,
+                                friendName: userName1
                                 };
-                            return $firebaseArray(ref.child('rooms').child(roomId).child('messages')).$loaded().then(function(data){
-                              console.log("return currentRoom.$add(newMessage);");
-                              console.log(data);
-                              return data.$add(newMessage);
+                                return user2.$add(newRoom2).then(function(){
+                                  var newMessage={
+                                      message: message,
+                                      userId: uid1
+                                      };
+                                  return $firebaseArray(ref.child('rooms').child(roomId).child('messages')).$loaded().then(function(data){
+                                    console.log("return currentRoom.$add(newMessage);");
+                                    console.log(data);
+                                    return data.$add(newMessage);
+                                  });
+
+                                });
+
+                              });
+
                             });
 
-                          });
+
 
                         });
-                        
-                        
-                
+
+
+
+
                       });
 
                     };
